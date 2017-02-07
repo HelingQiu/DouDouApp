0ddf163dde1babc5f4fc136812811e398d9dd14b
JK_iOS <Rainer@MacBook-Pro.local>
2017-02-07 20:49:03 +0800

    initilize
    豆豆停车项目启动

---
+++ DouDouStopCar/DouDouStopCar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DouDouStopCar
+//
+//  Created by Rainer on 17/2/7.
+//  Copyright © 2017年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
